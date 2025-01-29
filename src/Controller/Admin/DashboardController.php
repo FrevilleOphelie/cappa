f0b96d9c58e4ba2b3079bbfa91d7f2bb2be5eeb7
FrevilleOphelie <freville.ophelie@neuf.fr>
2025-01-29 16:22:02 +0100

    Controlleur admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Chats;
+use App\Entity\News;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -10,26 +13,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/adminouchats', name: 'adminouchats')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('adminouchats/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -40,7 +27,21 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        {
+            return [
+                MenuItem::linkToDashboard('Dashboard', 'fa-solid fa-arrow-rotate-left'),
+                MenuItem::linkToRoute('Retour au site', 'fa-solid fa-home', 'app_home'),
+
+                MenuItem::section('Chats'),
+                MenuItem::linkToCrud('Gestion des Pensionnaires', 'fa-solide fa-paw', Chats::class),
+
+                MenuItem::section('News'),
+                MenuItem::linkToCrud('Gestion des Nouvelles', 'fa-regular fa-newspaper', News::class),
+
+                MenuItem::section('User'),
+                MenuItem::linkToCrud('Gestion des Utilisateurs', 'fa-solid fa-user-group', User::class),
+
+            ];
+        }
     }
 }
