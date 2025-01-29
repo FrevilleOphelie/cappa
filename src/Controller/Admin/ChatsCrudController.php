ea1b21c61b49072b822bc29cd148a1fbef0e437c
FrevilleOphelie <freville.ophelie@neuf.fr>
2025-01-29 16:08:38 +0100

    CHatsCrudController

--- src/Controller/Admin/ChatsCrudController.php
+++ src/Controller/Admin/ChatsCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Chats;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -21,7 +22,11 @@ class ChatsCrudController extends AbstractCrudController
             IdField::new('id')
                 ->hideOnForm(),
             TextField::new('nom'),
-            TextField::new(''),
+            TextField::new('dateNaissance'),
+            TextField::new('description'),
+            BooleanField::new('adopte')->setLabel('Adopté')->renderAsSwitch(),
+            BooleanField::new('parraine')->setLabel('Parrainné')->renderAsSwitch(),
+            TextField::new('marraine'),
         ];
     }
 }
